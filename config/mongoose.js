813b77f1ef133a0f55b70c52b5a5e836ba72766d
Shruti <er.shrutimishra.2002@gmail.com>
2024-06-21 11:46:32 +0530

    New CHaanges

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 // const localDBUrl = "mongodb://127.0.0.1/suyog_db"
-const DBUrl = "mongodb+srv://electronicssuyog:Suyog%24%25%32%30%32%34%CB%86@cluster0.x5g8cd2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const DBUrl = "mongodb+srv://electronicssuyog:Suyog%24%252024%5E@cluster0.x5g8cd2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 // process.env.MONGODB_URL
 mongoose.connect(DBUrl)
 const db = mongoose.connection
